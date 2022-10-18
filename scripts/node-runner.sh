1b1b803565ad6c59455b50df291bf1a273987b43
Chris Gitmed <chris@ip-192-168-0-24.us-west-1.compute.internal>
2022-10-17 17:00:25 -0700

    Progress on node-runner.sh

--- scripts/node-runner.sh
+++ scripts/node-runner.sh
@@ -1,7 +1,26 @@
-# Runs a Node.js script, and afterwards logs the total run-time as well as
-# the peak memory pressure.
+# Runs a Node.js script, and writes logs from the script run, the total run-time,
+# as well as the peak memory pressure to a .txt file.
 # 
 # Invoke the script like this
-# sh node-runner.sh path-to-script/script.js --scriptArgs
+# sh node-runner.sh path-to-script/script.js scriptArgs
 
-# TODO (everything)
+# Validate at least one param has been passed
+if [[ ! -n $1 ]];
+then 
+  echo "No parameter passed."
+  exit 0
+fi
+
+# Get the script name
+# IFS='/'
+# read -ra new_arr <<< $1
+# echo "arr $new_arr"
+
+# echo "name ${new_arr[-1]}"
+
+# Assign path and arguments
+script_path=$1
+script_args=${@:2}
+
+# Run script and write to file
+node $script_path $script_args | tee test.txt
