08bbb9706aa885c61810a868ef0d28d374f5486f
Chris Gitmed <chris@Chriss-MacBook-Pro.local>
2022-12-21 17:05:34 -0800

    Updated create advent script to output typescript files with ES6 imports

--- scripts/create-node-advent.sh
+++ scripts/create-node-advent.sh
@@ -4,19 +4,24 @@
 # Invoke the script like this:
 # sh create-node-advent.sh dirName
 
-mkdir advent/$1 &&
-touch advent/$1/_.md advent/$1/_.test.js advent/$1/input.js advent/$1/solutions.js &&
+dirname="advent/$1"
+docfile="$dirname/_.md"
+testfile="$dirname/_.test.js"
+inputfile="$dirname/input.ts"
+solutionfile="$dirname/solutions.ts"
+mkdir $dirname &&
+touch $docfile $testfile $inputfile $solutionfile &&
 
 echo "# Day $1
 
 ## Part 1
 
-## Part 2" > advent/$1/_.md &&
+## Part 2" > $docfile &&
 
 echo "const { test, expect } = require('@jest/globals');
 
 const {  } = require('./solutions');
-const inputData = require('./input');
+const { inputData } = require('./input');
 
 test('', () => {
   expect((inputData)).toBe();
@@ -24,15 +29,17 @@ test('', () => {
 
 test('', () => {
   expect((inputData)).toBe();
-});" > advent/$1/_.test.js
+});" > $testfile
 
-echo "module.exports = \`\`;" > advent/$1/input.js
+echo "export const inputData = \`\`;" > $inputfile
 
 echo "
 // Part 1
+export const solutionOne = (input:string) => {
 
+};
 
 // Part 2
+export const solutionTwo = (input:string) => {
 
-
-module.exports = {  };" > advent/$1/solutions.js
+};" > $solutionfile
